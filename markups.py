2fdb654140fb53474168d4f0b7fc561a34d118f5
Firdavs Yusupov <84022174+firdavsyusupov@users.noreply.github.com>
2022-09-17 14:45:58 +0500

    Add files via upload

---
+++ markups.py
@@ -0,0 +1,156 @@
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+
+ds = InlineKeyboardButton('Data Science', callback_data='ds')
+fs = InlineKeyboardButton('Full Stack', callback_data='fs')
+se = InlineKeyboardButton('SoftWere Engineer', callback_data='se')
+
+btn_start = KeyboardButton('Reaktsiya qoldirish')
+btn_ds = KeyboardButton('Data Science')
+btn_fs = KeyboardButton('Full Stack')
+btn_se = KeyboardButton('Software Engineer')
+
+bad = InlineKeyboardButton('qoniqarsiz☹️', callback_data='bad')
+good = InlineKeyboardButton('qoniqarli😐', callback_data='good')
+great = InlineKeyboardButton('namunali😊', callback_data='great')
+
+bad_comment_1 = InlineKeyboardButton("Mentor o'z vaqtida ish joyida yo'q", callback_data='bc1')
+bad_comment_2 = InlineKeyboardButton("Mentor umuman yordam bera olmadi", callback_data='bc2')
+bad_comment_3 = InlineKeyboardButton("Mentor yordam berishdan bosh tortdi", callback_data='bc3')
+
+good_comment_1 = InlineKeyboardButton("Mentor ish joyiga kech keldi", callback_data='gdc1')
+good_comment_2 = InlineKeyboardButton("Mentor savolimga toliq jovob berolmadi", callback_data='gdc2')
+good_comment_3 = InlineKeyboardButton("Mentor javob berdi ammo muomilasizlik blan", callback_data='gdc3')
+
+great_comment_1 = InlineKeyboardButton("Mentor o'z vaqtida ish joyida", callback_data='gtc1')
+great_comment_2 = InlineKeyboardButton("Mentor barcha savoimga javob berdi", callback_data='gtc2')
+great_comment_3 = InlineKeyboardButton("Mentor juda ham yaxshi tushuntirdi", callback_data='gtc3')
+great_comment_4 = InlineKeyboardButton("Mentor yangicha va qiziqarli usulda taqdimot qilib tushuntirdi", callback_data='gtc4')
+
+mentor_se_1 = InlineKeyboardButton("Azodov Sarvar", callback_data='m1')
+mentor_se_2 = InlineKeyboardButton("Olloyorov Sirojiddin", callback_data='m2')
+mentor_fs_3 = InlineKeyboardButton("Rasulov Rahmatulloh", callback_data='m3')
+mentor_fs_4 = InlineKeyboardButton("Shomurodov Sarvarbek", callback_data='m4')
+mentor_fs_5 = InlineKeyboardButton("Shukurov Jasur", callback_data='m5')
+mentor_fs_6 = InlineKeyboardButton("Azizova Aziza", callback_data='m6')
+mentor_ds_7 = InlineKeyboardButton("Arslanova Nodira", callback_data='m7')
+mentor_ds_8 = InlineKeyboardButton("Alimbayeva Asalbonu", callback_data='m8')
+mentor_ds_9 = InlineKeyboardButton("Orifjonov Abdulaziz", callback_data='m9')
+
+#                   ________________________________________
+# ================================= ADMIN ===========================================
+#                   ________________________________________
+
+mentor_ase_1 = InlineKeyboardButton("Azodov Sarvar", callback_data='am1')
+mentor_ase_2 = InlineKeyboardButton("Olloyorov Sirojiddin", callback_data='am2')
+mentor_afs_3 = InlineKeyboardButton("Rasulov Rahmatulloh", callback_data='am3')
+mentor_afs_4 = InlineKeyboardButton("Shomurodov Sarvarbek", callback_data='am4')
+mentor_afs_5 = InlineKeyboardButton("Shukurov Jasur", callback_data='am5')
+mentor_afs_6 = InlineKeyboardButton("Azizova Aziza", callback_data='am6')
+mentor_ads_7 = InlineKeyboardButton("Arslanova Nodira", callback_data='am7')
+mentor_ads_8 = InlineKeyboardButton("Alimbayeva Asalbonu", callback_data='am8')
+mentor_ads_9 = InlineKeyboardButton("Orifjonov Abdulaziz", callback_data='am9')
+
+adm_mentors = InlineKeyboardButton('Barcha mentorlar', callback_data='adm_m')
+adm_ds_mentors = InlineKeyboardButton('Data Science', callback_data='adm_ds_m')
+adm_fs_mentors = InlineKeyboardButton('Full Stack', callback_data='adm_fs_m')
+adm_se_mentors = InlineKeyboardButton('Software Engineer', callback_data='adm_se_m')
+
+amentors = InlineKeyboardMarkup(row_width=1)
+amentors.insert(mentor_ase_1)
+amentors.insert(mentor_ase_2)
+amentors.insert(mentor_afs_3)
+amentors.insert(mentor_afs_4)
+amentors.insert(mentor_afs_5)
+amentors.insert(mentor_afs_6)
+amentors.insert(mentor_ads_7)
+amentors.insert(mentor_ads_8)
+amentors.insert(mentor_ads_9)
+
+
+adsmentors = InlineKeyboardMarkup(row_width=1)
+adsmentors.insert(mentor_ads_7)
+adsmentors.insert(mentor_ads_8)
+adsmentors.insert(mentor_ads_9)
+
+afsmentors = InlineKeyboardMarkup(row_width=1)
+afsmentors.insert(mentor_afs_3)
+#afsmentors.insert(mentor)
+
+adm_analysis = InlineKeyboardMarkup(row_width=1)
+adm_analysis.insert(adm_mentors)
+adm_analysis.insert(adm_ds_mentors)
+adm_analysis.insert(adm_ds_mentors)
+adm_analysis.insert(adm_se_mentors)
+
+btn_admBackMenu = KeyboardButton('Back')
+btn_week = KeyboardButton('Hafta')
+btn_day = KeyboardButton('Kun')
+
+btn_adminMenu = ReplyKeyboardMarkup(resize_keyboard=True)
+btn_adminMenu.add(btn_week)
+btn_adminMenu.add(btn_day)
+
+adm_menu = ReplyKeyboardMarkup(resize_keyboard=True)
+adm_menu.add(btn_admBackMenu)
+
+# ===================================================================================
+
+direction = InlineKeyboardMarkup(row_width=1)
+direction.insert(ds)
+direction.insert(fs)
+direction.insert(se)
+
+mainMenu = ReplyKeyboardMarkup(resize_keyboard=True)
+mainMenu.add(btn_start)
+
+directionMenu = ReplyKeyboardMarkup(resize_keyboard=True)
+directionMenu.add(btn_ds)
+directionMenu.add(btn_fs)
+directionMenu.add(btn_se)
+
+estimation = InlineKeyboardMarkup(row_width=1)
+estimation.insert(bad)
+estimation.insert(good)
+estimation.insert(great)
+
+bad_comment = InlineKeyboardMarkup(row_width=1)
+bad_comment.insert(bad_comment_1)
+bad_comment.insert(bad_comment_2)
+bad_comment.insert(bad_comment_3)
+
+good_comment = InlineKeyboardMarkup(row_width=1)
+good_comment.insert(good_comment_1)
+good_comment.insert(good_comment_2)
+good_comment.insert(good_comment_3)
+
+great_comment = InlineKeyboardMarkup(row_width=1)
+great_comment.insert(great_comment_1)
+great_comment.insert(great_comment_2)
+great_comment.insert(great_comment_3)
+great_comment.insert(great_comment_4)
+
+se_mentors = InlineKeyboardMarkup(row_width=1)
+se_mentors.insert(mentor_se_1)
+se_mentors.insert(mentor_se_2)
+
+fs_mentors = InlineKeyboardMarkup(row_width=1)
+fs_mentors.insert(mentor_fs_3)
+fs_mentors.insert(mentor_fs_4)
+fs_mentors.insert(mentor_fs_5)
+fs_mentors.insert(mentor_fs_6)
+
+ds_mentors = InlineKeyboardMarkup(row_width=1)
+ds_mentors.insert(mentor_ds_7)
+ds_mentors.insert(mentor_ds_8)
+ds_mentors.insert(mentor_ds_9)
+
+mentors = InlineKeyboardMarkup(row_width=1)
+mentors.insert(mentor_se_1)
+mentors.insert(mentor_se_2)
+mentors.insert(mentor_fs_3)
+mentors.insert(mentor_fs_4)
+mentors.insert(mentor_fs_5)
+mentors.insert(mentor_fs_6)
+mentors.insert(mentor_ds_7)
+mentors.insert(mentor_ds_8)
+mentors.insert(mentor_ds_9)
---
+++ sql_1.py
@@ -0,0 +1,139 @@
+import sqlite3
+import datetime
+import pandas as pd
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+
+class Databasee:
+    def __init__(self, db_file):
+        self.connection = sqlite3.connect(db_file)
+        self.cursor = self.connection.cursor()
+
+    def add_user(self, user_id):
+        with self.connection:
+            return self.cursor.execute("INSERT INTO 'users' ('user_id') VALUES (?)", (user_id,))
+
+    def user_exists(self, user_id):
+        with self.connection:
+            result = self.cursor.execute("SELECT * FROM 'users' WHERE 'user_id' = ?", (user_id,)).fetchall()
+            return bool(len(result))
+
+    # def get_direction(self, user_id, direction):
+    #     with self.connection:
+    #         result = self.cursor.execute("SELECT direction FROM users WHERE user_id = ?", (user_id,)).fetchall()
+    #         for row in result:
+    #             signup = str(row[0])
+    #         return signup
+    #
+    # def set_direction(self, user_id, direction):
+    #     with self.connection:
+    #         return self.cursor.execute("UPDATE users SET direction = ? WHERE user_id = ?", (direction, user_id,))
+
+    def get_directions(self, user_id):
+        with self.connection:
+            result = self.cursor.execute("SELECT direction FROM users WHERE user_id = ?", (user_id,)).fetchall()
+            for row in result:
+                signup = str(row[0])
+            return signup
+
+    def set_directions(self, user_id, direction):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET direction = ? WHERE user_id = ?", (direction, user_id,))
+
+    def get_signup(self, user_id):
+        with self.connection:
+            result = self.cursor.execute("SELECT signup FROM users WHERE user_id = ?", (user_id,)).fetchall()
+            for row in result:
+                signup = str(row[0])
+            return signup
+
+    def set_signup(self, user_id, signup):
+        with self.connection:
+            return self.cursor.execute("UPDATE users SET signup = ? WHERE user_id = ?", (signup, user_id,))
+
+    # =================================================================================================================
+
+    def add_users(self, user_id):
+        with self.connection:
+            return self.cursor.execute("INSERT INTO 'mentorss' ('user_id') VALUES (?)", (user_id,))
+
+    def date_time(self, user_id, date):
+        with self.connection:
+            return self.cursor.execute("UPDATE mentorss SET datetimes = ? WHERE 'user_id'=? ", (date, user_id,))
+
+    def add_mentor(self, user_id, mentor):
+        with self.connection:
+            return self.cursor.execute("UPDATE mentorss SET mentorlar = ? WHERE user_id=? ", (mentor, user_id,))
+
+    def add_qoniqarsiz(self, user_id, qoniqarsiz):
+        with self.connection:
+            return self.cursor.execute("UPDATE mentorss SET qoniqarsiz = ? WHERE user_id=? ", (qoniqarsiz, user_id,))
+
+    def add_qoniqarli(self, user_id, qoniqarli):
+        with self.connection:
+            return self.cursor.execute("UPDATE mentorss SET qoniqarli = ? WHERE user_id=? ", (qoniqarli, user_id,))
+
+    def add_namunali(self, user_id, namunali):
+        with self.connection:
+            return self.cursor.execute("UPDATE mentorss SET namunali = ? WHERE user_id=? ", (namunali, user_id,))
+
+    def add_limit(self, user_id, limit):
+        with self.connection:
+            return self.cursor.execute("UPDATE mentorss SET limited = ? WHERE user_id = ?", (limit, user_id))
+
+    def get_limit(self, user_id):
+        with self.connection:
+            result = self.cursor.execute("SELECT limited FROM mentorss WHERE user_id = ?", (user_id,)).fetchall()
+            for row in result:
+                signup = str(row[0])
+            return signup
+
+    def add_lim(self, user_id, limit):
+        with self.connection:
+            result = self.cursor.execute("[delete] or [update][set limited = 'val'] from < mentorss > where < user_id", (user_id,)).fetchall()
+            for row in result:
+                signup = str(row[0])
+            return signup
+
+
+
+    # def date_time(self, user_id, date):
+    #     with self.connection:
+    #         return self.cursor.execute(f"INSERT INTO mentorss VALUES ({date}) WHERE mentorss.user_id = user_id ")
+    #  # cursor.execute(f"INSERT INTO {today} VALUES ('{name}', '{product_name}', '{price}', '{phone}', '{description}', '{product_url}')")
+    # def add_mentor(sself, user_id, mentor)s:
+    #     with self.connection:
+    #         return self.cursor.execute(f"INSERT INTO mentorss VALUES ({mentor}s)  ")
+    #
+    # def add_qoniqarsiz(self, user_id, qoniqarsiz):
+    #     with self.connection:
+    #         return self.cursor.execute("INSERT INTO mentorss SET qoniqarsiz = ? WHERE user_id=? ", (qoniqarsiz, user_id,))
+    #
+    # def add_qoniqarli(self, user_id, qoniqarli):
+    #     with self.connection:
+    #         return self.cursor.execute("INSERT INTO mentorss SET qoniqarli = ? WHERE user_id=? ", (qoniqarli, user_id,))
+    #
+    # def add_namunali(self, user_id, namunali):
+    #     with self.connection:
+    #         return self.cursor.execute("INSERT INTO mentorss SET namunali = ? WHERE user_id=? ", (namunali, user_id,))
+
+
+
+
+# connection = sqlite3.connect('db_astrum (3).db')
+# cursor = connection.cursor()
+# db = Databasee('db_astrum (3).db')
+# def get_database():
+#     df = pd.read_sql(
+#         'SELECT [ID]\
+#         ,[user_id]\
+#         ,[datatimes]\
+#         ,[mentorlsar]\
+#         ,[qoniqarsiz]\
+#         ,[qoniqarli]\
+#         ,[namunali]\
+#     FROM mentorss',
+#     db,
+#         index_col='ID')
+#     print(df)
+# get_database()
